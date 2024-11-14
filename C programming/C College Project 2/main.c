25db329cb725fb3449013c74c7df7c53e0358214
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2024-11-14 11:45:25 +0000

    Update main.c

---
+++ C programming/C College Project 2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int array [10];
+    int i;
+
+for (i = 0; i < 10; i++) {
+ array[i] = i + 1;  
+}
+
+printf("The starting address of the array: %p\n")
+
+int index;
+
+printf("Enter an index between 0 and 9: ");
+
+scanf ("%d", index);
+
+if (index < 0 || index > 9) {
+    printf("Invalid index. please enter a number between 0 and 9.\n")
+    return 1;
+}
+
+printf ("")
+
+printf ("")
+
+
+return 0;
+} 
