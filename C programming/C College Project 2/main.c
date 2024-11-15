fadbb573945bd792f104246c9c1a6effd24fb503
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2024-11-15 10:17:44 +0000

    Update main.c
    works now

--- C programming/C College Project 2/main.c
+++ C programming/C College Project 2/main.c
@@ -9,13 +9,13 @@ for (i = 0; i < 10; i++) {
  array[i] = i + 1;  
 }
 
-printf("The starting address of the array: %p\n"(void*)&array);
+printf("The starting address of the array: %p\n", (void*)array);
 
 int index;
 
 printf("Enter an index between 0 and 9: ");
 
-scanf ("%d", index);
+scanf ("%d", &index);
 
 if (index < 0 || index > 9) {
     printf("Invalid index. Please enter a number between 0 and 9.\n");
